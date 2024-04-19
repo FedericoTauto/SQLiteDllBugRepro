089e1d1fa39194b97e7b20b855475be895c152b3
Federico Binda <f.binda@tautologics.cc>
2024-04-19 07:39:39 +0200

    initial commit

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿namespace BugRepro
+
+open FSharp.Data.Sql
+
+module Program =
+
+    let [<Literal>] private ConnectionString = "Data Source=" + __SOURCE_DIRECTORY__ + "/BugRepro.db;foreign keys=true"
+    let [<Literal>] private ResolutionPath = __SOURCE_DIRECTORY__ + @"/libs/win-x64"
+
+    type sqLite = SqlDataProvider<
+        Common.DatabaseProviderTypes.SQLITE,
+        SQLiteLibrary = Common.SQLiteLibrary.MicrosoftDataSqlite,
+        ConnectionString = ConnectionString, 
+        CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL,
+        ResolutionPath = ResolutionPath,
+        UseOptionTypes=Common.NullableColumnType.OPTION>
+
+    [<EntryPoint>]
+    let main _ = 
+        0
+    
